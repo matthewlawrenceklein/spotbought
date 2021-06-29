f76d3826e0526eb12d2220379fb13157ed7119ba
Matthew Klein <matthew@Borkbox-Jr.local>
2021-06-28 21:18:07 -0500

    chalk styling, new URL for vinyl query

--- spotBought.js
+++ spotBought.js
@@ -3,8 +3,19 @@ const spotify = require('spotify-node-applescript');
 const fetch = require("node-fetch");
 const cliProgress = require('cli-progress');
 var argv = require('minimist')(process.argv.slice(2));
+const chalk = require('chalk')
 const key = process.env.KEY
 const secret = process.env.SECRET
+
+// GLOBAL CHALK VAR
+const error = chalk.bold.red
+const friendly = chalk.bold.blue
+
+const progressBar = new cliProgress.SingleBar({
+    barCompleteChar: '\u2588',
+    barIncompleteChar: '\u2591',
+    hideCursor: true
+});
 
 if (argv.h || argv.help){
     console.log('spot bought is a buddy');
@@ -12,33 +23,30 @@ if (argv.h || argv.help){
 }
 
 spotify.getTrack(function(err, track){
-    const b1 = new cliProgress.SingleBar({
-        barCompleteChar: '\u2588',
-        barIncompleteChar: '\u2591',
-        hideCursor: true
-    });
-    
+
     let [artist, album] = [track.artist, track.album]
+    console.log(friendly(`Listening to ${album} by ${artist}`));
     if(err){
-        console.log("encountered an error :(", err);
+        console.log(error("encountered an error :(", err));
     } else {
-        b1.start(100, 0, {
+        progressBar.start(100, 0, {
             speed: "N/A"
         });
+        console.log(friendly(`Querying discogs for ${album}`));
         fetch(`https://api.discogs.com/database/search?artist=${artist}&release_title=${album}&key=${key}&secret=${secret}`)
             .then(resp => resp.json())
             .then(query => {
-                b1.increment(100)
+                progressBar.increment(100)
                 if(query.results[0]){
                     const release_id = query.results[0].master_id
                     fetch(`https://api.discogs.com/marketplace/stats/${release_id}?curr_abbr=${argv.c ? argv.c : 'USD'}`)
                         .then(resp => resp.json())
                         .then(stats => {
-                            b1.stop()
-                            const URL = `https://www.discogs.com/sell/list?master_id=${release_id}`
+                            progressBar.stop()
+                            const URL = `https://www.discogs.com/sell/list?master_id=${release_id}&format=Vinyl`
                             const outputStr = 
                             `There are currently ${stats.num_for_sale} copies of ${album} for sale, with a low price of ${stats.lowest_price.value} ${stats.lowest_price.currency}. \n 
-Here is a link to the album on Discogs' marketplace : ${URL}                  
+Here is a link to the album's vinyl listings on Discogs' marketplace : ${URL}                  
                             `
                             console.log(outputStr);
                         })
