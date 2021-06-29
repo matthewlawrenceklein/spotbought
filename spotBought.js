94e99d14e305d0f06a9897f278544b6844077577
Matthew Klein <matthew@Borkbox-Jr.local>
2021-06-28 20:58:25 -0500

    restructure, gitignore

--- spotBought.js
+++ spotBought.js
@@ -1,21 +1,26 @@
+require('dotenv').config()
 const spotify = require('spotify-node-applescript');
 const fetch = require("node-fetch");
+var argv = require('minimist')(process.argv.slice(2));
+const key = process.env.KEY
+const secret = process.env.SECRET
 
-const key = 'MjkmHtHBNMFKCWCzJyVM'
-const secret = 'efUykRGJyRXlqxStkSPiPSfqKQLrxllW'
-
+if (argv.h || argv.help){
+    console.log('spot bought is a buddy');
+    return
+}
 
 spotify.getTrack(function(err, track){
     let [artist, album] = [track.artist, track.album]
     if(err){
-        console.log("theres been an error :(", err);
+        console.log("encountered an error :(", err);
     } else {
         fetch(`https://api.discogs.com/database/search?artist=${artist}&release_title=${album}&key=${key}&secret=${secret}`)
             .then(resp => resp.json())
             .then(query => {
                 if(query.results[0]){
                     const release_id = query.results[0].master_id
-                    fetch(`https://api.discogs.com/marketplace/stats/${release_id}`)
+                    fetch(`https://api.discogs.com/marketplace/stats/${release_id}?curr_abbr=${argv.c ? argv.c : 'USD'}`)
                         .then(resp => resp.json())
                         .then(stats => {
                             const URL = `https://www.discogs.com/sell/list?master_id=${release_id}`
